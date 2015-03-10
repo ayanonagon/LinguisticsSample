40f548341a7055d0f39fc2d0de9394ef814040a6
Ayaka Nonaka <ayanonagon@gmail.com>
2015-03-09 23:10:09 -0700

    Add project files

---
+++ LinguisticsSample/LinguisticsSample.h
@@ -0,0 +1,19 @@
+//
+//  LinguisticsSample.h
+//  LinguisticsSample
+//
+//  Created by Ayaka Nonaka on 3/9/15.
+//  Copyright (c) 2015 Ayaka Nonaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinguisticsSample.
+FOUNDATION_EXPORT double LinguisticsSampleVersionNumber;
+
+//! Project version string for LinguisticsSample.
+FOUNDATION_EXPORT const unsigned char LinguisticsSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinguisticsSample/PublicHeader.h>
+
+
